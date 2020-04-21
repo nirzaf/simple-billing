cd3c7ad277ad1473671556d0dde9db064d38823f
Mohamed Fazrin <mohamed.fazrin@staff.broadbandsolutions.com.au>
2020-04-22 01:06:37 +0530

    Add project files.

---
+++ SimpleBilling/Main.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace SimpleBilling
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.mASTERToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.manageItemsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.manageCustomersToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.manageInvoiceToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mASTERToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(12, 3, 0, 3);
+            this.menuStrip1.Size = new System.Drawing.Size(1362, 29);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // mASTERToolStripMenuItem
+            // 
+            this.mASTERToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.manageItemsToolStripMenuItem,
+            this.manageCustomersToolStripMenuItem,
+            this.manageInvoiceToolStripMenuItem});
+            this.mASTERToolStripMenuItem.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mASTERToolStripMenuItem.Name = "mASTERToolStripMenuItem";
+            this.mASTERToolStripMenuItem.Size = new System.Drawing.Size(89, 23);
+            this.mASTERToolStripMenuItem.Text = "MASTER";
+            // 
+            // manageItemsToolStripMenuItem
+            // 
+            this.manageItemsToolStripMenuItem.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.manageItemsToolStripMenuItem.Name = "manageItemsToolStripMenuItem";
+            this.manageItemsToolStripMenuItem.Size = new System.Drawing.Size(226, 24);
+            this.manageItemsToolStripMenuItem.Text = "Manage Items";
+            this.manageItemsToolStripMenuItem.Click += new System.EventHandler(this.manageItemsToolStripMenuItem_Click);
+            // 
+            // manageCustomersToolStripMenuItem
+            // 
+            this.manageCustomersToolStripMenuItem.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.manageCustomersToolStripMenuItem.Name = "manageCustomersToolStripMenuItem";
+            this.manageCustomersToolStripMenuItem.Size = new System.Drawing.Size(226, 24);
+            this.manageCustomersToolStripMenuItem.Text = "Manage Customers";
+            // 
+            // manageInvoiceToolStripMenuItem
+            // 
+            this.manageInvoiceToolStripMenuItem.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.manageInvoiceToolStripMenuItem.Name = "manageInvoiceToolStripMenuItem";
+            this.manageInvoiceToolStripMenuItem.Size = new System.Drawing.Size(226, 24);
+            this.manageInvoiceToolStripMenuItem.Text = "Manage Invoice";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 22F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1362, 741);
+            this.Controls.Add(this.menuStrip1);
+            this.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.Name = "Main";
+            this.Text = "Main";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem mASTERToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem manageItemsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem manageCustomersToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem manageInvoiceToolStripMenuItem;
+    }
+}
---
+++ SimpleBilling/Main.cs
@@ -0,0 +1,32 @@
+﻿using SimpleBilling.MasterForms;
+using System;
+using System.Windows.Forms;
+
+namespace SimpleBilling
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void manageItemsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            foreach (Form form in Application.OpenForms)
+            {
+                // If frmHome is Opened, set focus to it and exit subroutine.
+                if (form.GetType() == typeof(ManageItems))
+                {
+
+                    form.Activate();
+                    return;
+                }
+            }
+
+            ManageItems manageItems = new ManageItems();
+            manageItems.MdiParent = this;
+            manageItems.Show();
+        }
+    }
+}
