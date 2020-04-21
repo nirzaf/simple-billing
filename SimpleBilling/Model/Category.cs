6641a493a4407d14a59ffd34c7d48256176d9895
Mohamed Fazrin <mohamed.fazrin@staff.broadbandsolutions.com.au>
2020-04-22 02:23:17 +0530

    add some models

---
+++ SimpleBilling/Model/Category.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleBilling.Model
+{
+    class Category
+    {
+        [Key]
+        public int CategoryId { get; set; }
+
+        [MaxLength(150)]
+        public string CategoryName { get; set; }
+    }
+}
---
+++ SimpleBilling/Model/Item.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleBilling.Model
+{
+    class Item
+    {
+        [Key]
+        public int Id { get; set; }
+        public string  Code { get; set; }
+        public string ItemName { get; set; }
+        public string Unit { get; set; }
+        public string Barcode { get; set; }
+        public Category Categories { get; set; }
+
+
+    }
+}
