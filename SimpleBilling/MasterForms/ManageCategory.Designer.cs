5855d339af0d5d5b8a6d91de54eaea570c3679b9
Mohamed Fazrin <mohamed.fazrin@staff.broadbandsolutions.com.au>
2020-04-24 06:28:20 +0530

    Category added

---
+++ SimpleBilling/MasterForms/ManageCategory.Designer.cs
@@ -0,0 +1,280 @@
+﻿namespace SimpleBilling.MasterForms
+{
+    partial class ManageCategory
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.DGVCategories = new System.Windows.Forms.DataGridView();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.PanelCRUD = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TxtCatId = new System.Windows.Forms.TextBox();
+            this.TxtCategoryName = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.BtnAdd = new System.Windows.Forms.Button();
+            this.BtnEdit = new System.Windows.Forms.Button();
+            this.BtnDelete = new System.Windows.Forms.Button();
+            this.BtnSave = new System.Windows.Forms.Button();
+            this.LblMessage = new System.Windows.Forms.Label();
+            this.categoryBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.categoryIdDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.categoryNameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.DGVCategories)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.PanelCRUD.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.categoryBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // DGVCategories
+            // 
+            this.DGVCategories.AutoGenerateColumns = false;
+            this.DGVCategories.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.DGVCategories.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.DGVCategories.BackgroundColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.DGVCategories.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DGVCategories.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.categoryIdDataGridViewTextBoxColumn,
+            this.categoryNameDataGridViewTextBoxColumn});
+            this.DGVCategories.DataSource = this.categoryBindingSource;
+            this.DGVCategories.Location = new System.Drawing.Point(14, 18);
+            this.DGVCategories.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.DGVCategories.Name = "DGVCategories";
+            this.DGVCategories.Size = new System.Drawing.Size(359, 208);
+            this.DGVCategories.TabIndex = 0;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 28.99408F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 71.00592F));
+            this.tableLayoutPanel1.Controls.Add(this.TxtCategoryName, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.TxtCatId, 1, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(23, 20);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(477, 63);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // PanelCRUD
+            // 
+            this.PanelCRUD.Controls.Add(this.tableLayoutPanel1);
+            this.PanelCRUD.Location = new System.Drawing.Point(383, 18);
+            this.PanelCRUD.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.PanelCRUD.Name = "PanelCRUD";
+            this.PanelCRUD.Size = new System.Drawing.Size(507, 97);
+            this.PanelCRUD.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(5, 0);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Category Id";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(5, 30);
+            this.label2.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(127, 19);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Category Name";
+            // 
+            // TxtCatId
+            // 
+            this.TxtCatId.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.categoryBindingSource, "CategoryId", true));
+            this.TxtCatId.Location = new System.Drawing.Point(141, 3);
+            this.TxtCatId.Name = "TxtCatId";
+            this.TxtCatId.ReadOnly = true;
+            this.TxtCatId.Size = new System.Drawing.Size(333, 26);
+            this.TxtCatId.TabIndex = 2;
+            // 
+            // TxtCategoryName
+            // 
+            this.TxtCategoryName.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.categoryBindingSource, "CategoryName", true));
+            this.TxtCategoryName.Location = new System.Drawing.Point(141, 33);
+            this.TxtCategoryName.Name = "TxtCategoryName";
+            this.TxtCategoryName.Size = new System.Drawing.Size(333, 26);
+            this.TxtCategoryName.TabIndex = 3;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 4;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel2.Controls.Add(this.BtnSave, 3, 0);
+            this.tableLayoutPanel2.Controls.Add(this.BtnDelete, 2, 0);
+            this.tableLayoutPanel2.Controls.Add(this.BtnEdit, 1, 0);
+            this.tableLayoutPanel2.Controls.Add(this.BtnAdd, 0, 0);
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(383, 122);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(477, 64);
+            this.tableLayoutPanel2.TabIndex = 2;
+            // 
+            // BtnAdd
+            // 
+            this.BtnAdd.BackColor = System.Drawing.Color.White;
+            this.BtnAdd.ForeColor = System.Drawing.Color.Black;
+            this.BtnAdd.Location = new System.Drawing.Point(3, 3);
+            this.BtnAdd.Name = "BtnAdd";
+            this.BtnAdd.Size = new System.Drawing.Size(113, 58);
+            this.BtnAdd.TabIndex = 0;
+            this.BtnAdd.Text = "Add";
+            this.BtnAdd.UseVisualStyleBackColor = false;
+            this.BtnAdd.Click += new System.EventHandler(this.BtnAdd_Click);
+            // 
+            // BtnEdit
+            // 
+            this.BtnEdit.BackColor = System.Drawing.Color.White;
+            this.BtnEdit.ForeColor = System.Drawing.Color.Black;
+            this.BtnEdit.Location = new System.Drawing.Point(122, 3);
+            this.BtnEdit.Name = "BtnEdit";
+            this.BtnEdit.Size = new System.Drawing.Size(113, 58);
+            this.BtnEdit.TabIndex = 1;
+            this.BtnEdit.Text = "Edit";
+            this.BtnEdit.UseVisualStyleBackColor = false;
+            this.BtnEdit.Click += new System.EventHandler(this.BtnEdit_Click);
+            // 
+            // BtnDelete
+            // 
+            this.BtnDelete.BackColor = System.Drawing.Color.White;
+            this.BtnDelete.ForeColor = System.Drawing.Color.Black;
+            this.BtnDelete.Location = new System.Drawing.Point(241, 3);
+            this.BtnDelete.Name = "BtnDelete";
+            this.BtnDelete.Size = new System.Drawing.Size(113, 58);
+            this.BtnDelete.TabIndex = 2;
+            this.BtnDelete.Text = "Delete";
+            this.BtnDelete.UseVisualStyleBackColor = false;
+            this.BtnDelete.Click += new System.EventHandler(this.BtnDelete_Click);
+            // 
+            // BtnSave
+            // 
+            this.BtnSave.BackColor = System.Drawing.Color.White;
+            this.BtnSave.ForeColor = System.Drawing.Color.Black;
+            this.BtnSave.Location = new System.Drawing.Point(360, 3);
+            this.BtnSave.Name = "BtnSave";
+            this.BtnSave.Size = new System.Drawing.Size(113, 58);
+            this.BtnSave.TabIndex = 3;
+            this.BtnSave.Text = "Save";
+            this.BtnSave.UseVisualStyleBackColor = false;
+            this.BtnSave.Click += new System.EventHandler(this.BtnSave_Click);
+            // 
+            // LblMessage
+            // 
+            this.LblMessage.AutoSize = true;
+            this.LblMessage.ForeColor = System.Drawing.Color.Lime;
+            this.LblMessage.Location = new System.Drawing.Point(14, 248);
+            this.LblMessage.Name = "LblMessage";
+            this.LblMessage.Size = new System.Drawing.Size(54, 19);
+            this.LblMessage.TabIndex = 3;
+            this.LblMessage.Text = "label3";
+            // 
+            // categoryBindingSource
+            // 
+            this.categoryBindingSource.DataSource = typeof(SimpleBilling.Model.Category);
+            // 
+            // categoryIdDataGridViewTextBoxColumn
+            // 
+            this.categoryIdDataGridViewTextBoxColumn.DataPropertyName = "CategoryId";
+            this.categoryIdDataGridViewTextBoxColumn.HeaderText = "CategoryId";
+            this.categoryIdDataGridViewTextBoxColumn.Name = "categoryIdDataGridViewTextBoxColumn";
+            this.categoryIdDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // categoryNameDataGridViewTextBoxColumn
+            // 
+            this.categoryNameDataGridViewTextBoxColumn.DataPropertyName = "CategoryName";
+            this.categoryNameDataGridViewTextBoxColumn.HeaderText = "CategoryName";
+            this.categoryNameDataGridViewTextBoxColumn.Name = "categoryNameDataGridViewTextBoxColumn";
+            // 
+            // ManageCategory
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(897, 290);
+            this.Controls.Add(this.tableLayoutPanel2);
+            this.Controls.Add(this.LblMessage);
+            this.Controls.Add(this.PanelCRUD);
+            this.Controls.Add(this.DGVCategories);
+            this.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.Black;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.Name = "ManageCategory";
+            this.Text = "Manage Category";
+            this.Load += new System.EventHandler(this.ManageCategory_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.DGVCategories)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.PanelCRUD.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.categoryBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView DGVCategories;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TextBox TxtCategoryName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox TxtCatId;
+        private System.Windows.Forms.Panel PanelCRUD;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Button BtnSave;
+        private System.Windows.Forms.Button BtnDelete;
+        private System.Windows.Forms.Button BtnEdit;
+        private System.Windows.Forms.Button BtnAdd;
+        private System.Windows.Forms.BindingSource categoryBindingSource;
+        private System.Windows.Forms.DataGridViewTextBoxColumn categoryIdDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn categoryNameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.Label LblMessage;
+    }
+}
---
+++ SimpleBilling/MasterForms/ManageCategory.cs
@@ -0,0 +1,117 @@
+﻿using SimpleBilling.Model;
+using System;
+using System.Data.Entity;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace SimpleBilling.MasterForms
+{
+    public partial class ManageCategory : Form
+    {
+        public ManageCategory()
+        {
+            InitializeComponent();
+        }
+
+        private void ManageCategory_Load(object sender, EventArgs e)
+        {
+            LoadDGV();
+        }
+
+        private void LoadDGV()
+        {
+            PanelCRUD.Enabled = false;
+            using (BillingContext db = new BillingContext())
+            {
+                categoryBindingSource.DataSource = db.Categories.ToList();
+            }
+        }
+
+        private void BtnAdd_Click(object sender, EventArgs e)
+        {
+            PanelCRUD.Enabled = true;
+            BtnSave.Enabled = true;
+            categoryBindingSource.Add(new Category());
+            categoryBindingSource.MoveLast();
+            TxtCategoryName.Focus();
+        }
+
+        private void BtnEdit_Click(object sender, EventArgs e)
+        {
+            PanelCRUD.Enabled = true;
+            BtnSave.Enabled = true;
+            TxtCategoryName.Focus();
+            Category cat = categoryBindingSource.Current as Category;
+
+        }
+
+        private void BtnDelete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DialogResult dialogResult = MessageBox.Show("Are you sure you want to delete the selected Item?", "Confirmation delete", MessageBoxButtons.YesNo);
+                if (dialogResult == DialogResult.Yes)
+                {
+                    using (BillingContext db = new BillingContext())
+                    {
+                        Category cat = categoryBindingSource.Current as Category;
+
+                        if (cat != null)
+                        {
+                            if (db.Entry(cat).State ==EntityState.Detached)
+                                db.Set<Category>().Attach(cat);
+                            db.Entry(cat).State = EntityState.Deleted;
+                            db.SaveChanges();
+                            Info("Category Deleted Successfully");
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Info(ex.ToString());
+            }
+            finally
+            {
+                DGVCategories.Refresh();
+                LoadDGV();
+            }
+        }
+
+        private void Info(string Message)
+        {
+            LblMessage.Text = Message;
+        }
+
+        private void BtnSave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (BillingContext db = new BillingContext())
+                {
+                    if (categoryBindingSource.Current is Category cat)
+                    {
+                        if (db.Entry(cat).State == EntityState.Detached)
+                            db.Set<Category>().Attach(cat);
+                        if (cat.CategoryId == 0)
+                        {
+                            db.Entry(cat).State = EntityState.Added;
+                            Info("Category Added");
+                        }
+                        else 
+                        {
+                            db.Entry(cat).State = EntityState.Modified;
+                            Info("Category Modified");
+                        }
+                        db.SaveChanges();
+                    }
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
