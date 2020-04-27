624f27eaf381558015eff62de92ca452f9a7c242
Mohamed Fazrin <mohamed.fazrin@staff.broadbandsolutions.com.au>
2020-04-28 04:54:08 +0530

    cool

--- SimpleBilling/MasterForms/GRNInvoices.Designer.cs
+++ SimpleBilling/MasterForms/GRNInvoices.Designer.cs
@@ -63,7 +63,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.ClientSize = new System.Drawing.Size(939, 430);
+            this.ClientSize = new System.Drawing.Size(939, 408);
             this.Controls.Add(this.BtnLoadInvoice);
             this.Controls.Add(this.DGVInvoices);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
