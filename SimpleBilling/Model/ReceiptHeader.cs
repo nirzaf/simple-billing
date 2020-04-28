4aacedbb151b009f3d6991e91b0023d277558917
Mohamed Fazrin <mohamed.fazrin@staff.broadbandsolutions.com.au>
2020-04-28 06:45:44 +0530

    mingle

---
+++ SimpleBilling/Model/ReceiptHeader.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleBilling.Model
+{
+    class ReceiptHeader
+    {
+        [Key]
+        public int ReceiptId { get; set; }
+        [Required]
+        public string Date  { get; set; }
+        [Required]
+        public string Time { get; set; }
+        [Required]
+        public int Cashier { get; set; }
+        [DefaultValue(0)]
+        public float TotalDiscount { get; set; }
+        [DefaultValue(0)]       
+        public float TotalPrice { get; set; }
+
+    }
+}
