8c06bbf890e574b016702a745ae025178913a7e1
Mohamed Fazrin <mohamed.fazrin@staff.broadbandsolutions.com.au>
2020-04-28 09:16:24 +0530

    added model for receipts

---
+++ SimpleBilling/Model/ReceiptBody.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleBilling.Model
+{
+    public class ReceiptBody
+    {
+        [Key]
+        [Index(Order=1)]
+        public int ReceiptId { get; set; }
+        [Key]
+        [Index(Order=2)]
+        public int LineId { get; set; }
+        [Required]
+        public int ProductId { get; set; }
+        [Required]
+        public string ProductCode { get; set; }
+        [Required]
+        public string ProductName { get; set; }
+        [Required]
+        public float UnitPrice { get; set; }
+        [Required]
+        public int Quantity { get; set; }
+        [DefaultValue(0)]
+        public float Discount { get; set; }
+        public float SubTotal { get; set; }
+        [Required]
+        public float NetTotal { get; set; }
+
+    }
+}
--- SimpleBilling/Model/ReceiptHeader.cs
+++ SimpleBilling/Model/ReceiptHeader.cs
@@ -13,6 +13,9 @@ namespace SimpleBilling.Model
         [Key]
         public int ReceiptId { get; set; }
         [Required]
+        [MaxLength(200)]
+        public string ReceiptNo { get; set; }
+        [Required]
         public string Date  { get; set; }
         [Required]
         public string Time { get; set; }
@@ -20,8 +23,13 @@ namespace SimpleBilling.Model
         public int Cashier { get; set; }
         [DefaultValue(0)]
         public float TotalDiscount { get; set; }
+        public float SubTotal { get; set; }
         [DefaultValue(0)]       
-        public float TotalPrice { get; set; }
-
+        public float NetTotal { get; set; }
+        [MaxLength(10)]
+        public string PaymentType { get; set; }
+        public float PaidAmount { get; set; }
+        public float Balance { get; set; }
+        public int Status { get; set; }
     }
 }
