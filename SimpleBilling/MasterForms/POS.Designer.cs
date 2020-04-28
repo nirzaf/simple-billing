c97286d7fb9df1b29c484cda3d71fdf0cb5ab69c
Mohamed Fazrin <mohamed.fazrin@staff.broadbandsolutions.com.au>
2020-04-29 02:12:35 +0530

    submit

--- SimpleBilling/MasterForms/POS.Designer.cs
+++ SimpleBilling/MasterForms/POS.Designer.cs
@@ -32,11 +32,28 @@
             this.LblDate = new System.Windows.Forms.Label();
             this.LblTime = new System.Windows.Forms.Label();
             this.SystemTimer = new System.Windows.Forms.Timer(this.components);
-            this.CmbCustomers = new System.Windows.Forms.ComboBox();
             this.customersBindingSource = new System.Windows.Forms.BindingSource(this.components);
-            this.BtnAddCustomer = new System.Windows.Forms.Button();
             this.CmbAddItem = new System.Windows.Forms.ComboBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.TxtBarCode = new System.Windows.Forms.TextBox();
+            this.TxtProductCode = new System.Windows.Forms.TextBox();
+            this.TxtUnitPrice = new System.Windows.Forms.TextBox();
+            this.TxtQuantity = new System.Windows.Forms.TextBox();
+            this.TxtDiscount = new System.Windows.Forms.TextBox();
+            this.TxtSubTotal = new System.Windows.Forms.TextBox();
+            this.TxtNetTotal = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.TxtCustomer = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.customersBindingSource)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // LblDate
@@ -44,7 +61,7 @@
             this.LblDate.AutoSize = true;
             this.LblDate.BackColor = System.Drawing.SystemColors.ControlDarkDark;
             this.LblDate.ForeColor = System.Drawing.Color.Lime;
-            this.LblDate.Location = new System.Drawing.Point(12, 9);
+            this.LblDate.Location = new System.Drawing.Point(3, 0);
             this.LblDate.Name = "LblDate";
             this.LblDate.Size = new System.Drawing.Size(44, 19);
             this.LblDate.TabIndex = 0;
@@ -54,7 +71,7 @@
             // 
             this.LblTime.AutoSize = true;
             this.LblTime.ForeColor = System.Drawing.Color.Lime;
-            this.LblTime.Location = new System.Drawing.Point(99, 9);
+            this.LblTime.Location = new System.Drawing.Point(253, 0);
             this.LblTime.Name = "LblTime";
             this.LblTime.Size = new System.Drawing.Size(46, 19);
             this.LblTime.TabIndex = 1;
@@ -65,38 +82,210 @@
             this.SystemTimer.Enabled = true;
             this.SystemTimer.Tick += new System.EventHandler(this.SystemTimer_Tick);
             // 
-            // CmbCustomers
-            // 
-            this.CmbCustomers.DataBindings.Add(new System.Windows.Forms.Binding("SelectedValue", this.customersBindingSource, "CustomerId", true));
-            this.CmbCustomers.DataSource = this.customersBindingSource;
-            this.CmbCustomers.DisplayMember = "Name";
-            this.CmbCustomers.FormattingEnabled = true;
-            this.CmbCustomers.Location = new System.Drawing.Point(228, 6);
-            this.CmbCustomers.Name = "CmbCustomers";
-            this.CmbCustomers.Size = new System.Drawing.Size(187, 27);
-            this.CmbCustomers.TabIndex = 2;
-            this.CmbCustomers.ValueMember = "CustomerId";
-            // 
             // customersBindingSource
             // 
             this.customersBindingSource.DataSource = typeof(SimpleBilling.Model.Customers);
             // 
-            // BtnAddCustomer
-            // 
-            this.BtnAddCustomer.Location = new System.Drawing.Point(422, 6);
-            this.BtnAddCustomer.Name = "BtnAddCustomer";
-            this.BtnAddCustomer.Size = new System.Drawing.Size(50, 27);
-            this.BtnAddCustomer.TabIndex = 3;
-            this.BtnAddCustomer.Text = "Add";
-            this.BtnAddCustomer.UseVisualStyleBackColor = true;
-            // 
             // CmbAddItem
             // 
             this.CmbAddItem.FormattingEnabled = true;
-            this.CmbAddItem.Location = new System.Drawing.Point(479, 6);
+            this.CmbAddItem.Location = new System.Drawing.Point(503, 63);
             this.CmbAddItem.Name = "CmbAddItem";
-            this.CmbAddItem.Size = new System.Drawing.Size(225, 27);
+            this.CmbAddItem.Size = new System.Drawing.Size(244, 27);
             this.CmbAddItem.TabIndex = 4;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 4;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.Controls.Add(this.TxtCustomer, 3, 0);
+            this.tableLayoutPanel1.Controls.Add(this.CmbAddItem, 2, 2);
+            this.tableLayoutPanel1.Controls.Add(this.label9, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.LblDate, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.LblTime, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label3, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label4, 3, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label5, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.label6, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.label7, 2, 3);
+            this.tableLayoutPanel1.Controls.Add(this.label8, 3, 3);
+            this.tableLayoutPanel1.Controls.Add(this.TxtBarCode, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.TxtProductCode, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.TxtUnitPrice, 3, 2);
+            this.tableLayoutPanel1.Controls.Add(this.TxtQuantity, 0, 4);
+            this.tableLayoutPanel1.Controls.Add(this.TxtDiscount, 1, 4);
+            this.tableLayoutPanel1.Controls.Add(this.TxtSubTotal, 2, 4);
+            this.tableLayoutPanel1.Controls.Add(this.TxtNetTotal, 3, 4);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 5;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1003, 150);
+            this.tableLayoutPanel1.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label1.ForeColor = System.Drawing.Color.Lime;
+            this.label1.Location = new System.Drawing.Point(3, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 19);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Bar Code";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label2.ForeColor = System.Drawing.Color.Lime;
+            this.label2.Location = new System.Drawing.Point(253, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(115, 19);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Product Code";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label3.ForeColor = System.Drawing.Color.Lime;
+            this.label3.Location = new System.Drawing.Point(503, 30);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(133, 19);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Item Description";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label4.ForeColor = System.Drawing.Color.Lime;
+            this.label4.Location = new System.Drawing.Point(753, 30);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(83, 19);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Unit Price";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label5.ForeColor = System.Drawing.Color.Lime;
+            this.label5.Location = new System.Drawing.Point(3, 90);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(73, 19);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Quantity";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label6.ForeColor = System.Drawing.Color.Lime;
+            this.label6.Location = new System.Drawing.Point(253, 90);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(78, 19);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Discount";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label7.ForeColor = System.Drawing.Color.Lime;
+            this.label7.Location = new System.Drawing.Point(503, 90);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(81, 19);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "Sub Total";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label8.ForeColor = System.Drawing.Color.Lime;
+            this.label8.Location = new System.Drawing.Point(753, 90);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(76, 19);
+            this.label8.TabIndex = 9;
+            this.label8.Text = "Net Total";
+            // 
+            // TxtBarCode
+            // 
+            this.TxtBarCode.Location = new System.Drawing.Point(3, 63);
+            this.TxtBarCode.Name = "TxtBarCode";
+            this.TxtBarCode.Size = new System.Drawing.Size(244, 26);
+            this.TxtBarCode.TabIndex = 10;
+            // 
+            // TxtProductCode
+            // 
+            this.TxtProductCode.Location = new System.Drawing.Point(253, 63);
+            this.TxtProductCode.Name = "TxtProductCode";
+            this.TxtProductCode.Size = new System.Drawing.Size(244, 26);
+            this.TxtProductCode.TabIndex = 11;
+            // 
+            // TxtUnitPrice
+            // 
+            this.TxtUnitPrice.Location = new System.Drawing.Point(753, 63);
+            this.TxtUnitPrice.Name = "TxtUnitPrice";
+            this.TxtUnitPrice.Size = new System.Drawing.Size(244, 26);
+            this.TxtUnitPrice.TabIndex = 12;
+            // 
+            // TxtQuantity
+            // 
+            this.TxtQuantity.Location = new System.Drawing.Point(3, 123);
+            this.TxtQuantity.Name = "TxtQuantity";
+            this.TxtQuantity.Size = new System.Drawing.Size(244, 26);
+            this.TxtQuantity.TabIndex = 13;
+            // 
+            // TxtDiscount
+            // 
+            this.TxtDiscount.Location = new System.Drawing.Point(253, 123);
+            this.TxtDiscount.Name = "TxtDiscount";
+            this.TxtDiscount.Size = new System.Drawing.Size(244, 26);
+            this.TxtDiscount.TabIndex = 14;
+            // 
+            // TxtSubTotal
+            // 
+            this.TxtSubTotal.Location = new System.Drawing.Point(503, 123);
+            this.TxtSubTotal.Name = "TxtSubTotal";
+            this.TxtSubTotal.Size = new System.Drawing.Size(244, 26);
+            this.TxtSubTotal.TabIndex = 15;
+            // 
+            // TxtNetTotal
+            // 
+            this.TxtNetTotal.Location = new System.Drawing.Point(753, 123);
+            this.TxtNetTotal.Name = "TxtNetTotal";
+            this.TxtNetTotal.Size = new System.Drawing.Size(244, 26);
+            this.TxtNetTotal.TabIndex = 16;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label9.ForeColor = System.Drawing.Color.Lime;
+            this.label9.Location = new System.Drawing.Point(503, 0);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(84, 19);
+            this.label9.TabIndex = 17;
+            this.label9.Text = "Customer";
+            // 
+            // TxtCustomer
+            // 
+            this.TxtCustomer.Location = new System.Drawing.Point(753, 3);
+            this.TxtCustomer.Name = "TxtCustomer";
+            this.TxtCustomer.Size = new System.Drawing.Size(244, 26);
+            this.TxtCustomer.TabIndex = 18;
             // 
             // POS
             // 
@@ -104,11 +293,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
             this.ClientSize = new System.Drawing.Size(1027, 511);
-            this.Controls.Add(this.CmbAddItem);
-            this.Controls.Add(this.BtnAddCustomer);
-            this.Controls.Add(this.CmbCustomers);
-            this.Controls.Add(this.LblTime);
-            this.Controls.Add(this.LblDate);
+            this.Controls.Add(this.tableLayoutPanel1);
             this.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
@@ -117,8 +302,9 @@
             this.Text = "POS";
             this.Load += new System.EventHandler(this.POS_Load);
             ((System.ComponentModel.ISupportInitialize)(this.customersBindingSource)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -127,9 +313,25 @@
         private System.Windows.Forms.Label LblDate;
         private System.Windows.Forms.Label LblTime;
         private System.Windows.Forms.Timer SystemTimer;
-        private System.Windows.Forms.ComboBox CmbCustomers;
         private System.Windows.Forms.BindingSource customersBindingSource;
-        private System.Windows.Forms.Button BtnAddCustomer;
         private System.Windows.Forms.ComboBox CmbAddItem;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox TxtCustomer;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox TxtBarCode;
+        private System.Windows.Forms.TextBox TxtProductCode;
+        private System.Windows.Forms.TextBox TxtUnitPrice;
+        private System.Windows.Forms.TextBox TxtQuantity;
+        private System.Windows.Forms.TextBox TxtDiscount;
+        private System.Windows.Forms.TextBox TxtSubTotal;
+        private System.Windows.Forms.TextBox TxtNetTotal;
     }
 }
