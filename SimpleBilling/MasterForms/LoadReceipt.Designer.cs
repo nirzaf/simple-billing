b3c105f3e97bdcadc417afd22731bc01af9305f7
Mohamed Fazrin <mohamed.fazrin@staff.broadbandsolutions.com.au>
2020-04-30 03:40:35 +0530

    kandippa madam

--- SimpleBilling/MasterForms/LoadReceipt.Designer.cs
+++ SimpleBilling/MasterForms/LoadReceipt.Designer.cs
@@ -28,19 +28,145 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.DGVLoadReceipt = new System.Windows.Forms.DataGridView();
+            this.BtnLoadReceipt = new System.Windows.Forms.Button();
+            this.receiptHeaderBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.receiptNoDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dateDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.timeDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.totalDiscountDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.subTotalDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.netTotalDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.paidAmountDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.balanceDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.DGVLoadReceipt)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.receiptHeaderBindingSource)).BeginInit();
             this.SuspendLayout();
+            // 
+            // DGVLoadReceipt
+            // 
+            this.DGVLoadReceipt.AutoGenerateColumns = false;
+            this.DGVLoadReceipt.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.ColumnHeader;
+            this.DGVLoadReceipt.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.DGVLoadReceipt.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DGVLoadReceipt.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.receiptNoDataGridViewTextBoxColumn,
+            this.dateDataGridViewTextBoxColumn,
+            this.timeDataGridViewTextBoxColumn,
+            this.totalDiscountDataGridViewTextBoxColumn,
+            this.subTotalDataGridViewTextBoxColumn,
+            this.netTotalDataGridViewTextBoxColumn,
+            this.paidAmountDataGridViewTextBoxColumn,
+            this.balanceDataGridViewTextBoxColumn});
+            this.DGVLoadReceipt.DataSource = this.receiptHeaderBindingSource;
+            this.DGVLoadReceipt.Location = new System.Drawing.Point(2, 29);
+            this.DGVLoadReceipt.Name = "DGVLoadReceipt";
+            this.DGVLoadReceipt.Size = new System.Drawing.Size(965, 438);
+            this.DGVLoadReceipt.TabIndex = 0;
+            // 
+            // BtnLoadReceipt
+            // 
+            this.BtnLoadReceipt.BackColor = System.Drawing.Color.White;
+            this.BtnLoadReceipt.Location = new System.Drawing.Point(810, 473);
+            this.BtnLoadReceipt.Name = "BtnLoadReceipt";
+            this.BtnLoadReceipt.Size = new System.Drawing.Size(157, 33);
+            this.BtnLoadReceipt.TabIndex = 1;
+            this.BtnLoadReceipt.Text = "Load Receipt";
+            this.BtnLoadReceipt.UseVisualStyleBackColor = false;
+            // 
+            // receiptHeaderBindingSource
+            // 
+            this.receiptHeaderBindingSource.DataSource = typeof(SimpleBilling.Model.ReceiptHeader);
+            // 
+            // receiptNoDataGridViewTextBoxColumn
+            // 
+            this.receiptNoDataGridViewTextBoxColumn.DataPropertyName = "ReceiptNo";
+            this.receiptNoDataGridViewTextBoxColumn.HeaderText = "Receipt No";
+            this.receiptNoDataGridViewTextBoxColumn.Name = "receiptNoDataGridViewTextBoxColumn";
+            this.receiptNoDataGridViewTextBoxColumn.Width = 118;
+            // 
+            // dateDataGridViewTextBoxColumn
+            // 
+            this.dateDataGridViewTextBoxColumn.DataPropertyName = "Date";
+            this.dateDataGridViewTextBoxColumn.HeaderText = "Date";
+            this.dateDataGridViewTextBoxColumn.Name = "dateDataGridViewTextBoxColumn";
+            this.dateDataGridViewTextBoxColumn.Width = 69;
+            // 
+            // timeDataGridViewTextBoxColumn
+            // 
+            this.timeDataGridViewTextBoxColumn.DataPropertyName = "Time";
+            this.timeDataGridViewTextBoxColumn.HeaderText = "Time";
+            this.timeDataGridViewTextBoxColumn.Name = "timeDataGridViewTextBoxColumn";
+            this.timeDataGridViewTextBoxColumn.Width = 71;
+            // 
+            // totalDiscountDataGridViewTextBoxColumn
+            // 
+            this.totalDiscountDataGridViewTextBoxColumn.DataPropertyName = "TotalDiscount";
+            this.totalDiscountDataGridViewTextBoxColumn.HeaderText = "Total Discount";
+            this.totalDiscountDataGridViewTextBoxColumn.Name = "totalDiscountDataGridViewTextBoxColumn";
+            this.totalDiscountDataGridViewTextBoxColumn.Width = 132;
+            // 
+            // subTotalDataGridViewTextBoxColumn
+            // 
+            this.subTotalDataGridViewTextBoxColumn.DataPropertyName = "SubTotal";
+            this.subTotalDataGridViewTextBoxColumn.HeaderText = "Sub Total";
+            this.subTotalDataGridViewTextBoxColumn.Name = "subTotalDataGridViewTextBoxColumn";
+            this.subTotalDataGridViewTextBoxColumn.Width = 97;
+            // 
+            // netTotalDataGridViewTextBoxColumn
+            // 
+            this.netTotalDataGridViewTextBoxColumn.DataPropertyName = "NetTotal";
+            this.netTotalDataGridViewTextBoxColumn.HeaderText = "Net Total";
+            this.netTotalDataGridViewTextBoxColumn.Name = "netTotalDataGridViewTextBoxColumn";
+            this.netTotalDataGridViewTextBoxColumn.Width = 93;
+            // 
+            // paidAmountDataGridViewTextBoxColumn
+            // 
+            this.paidAmountDataGridViewTextBoxColumn.DataPropertyName = "PaidAmount";
+            this.paidAmountDataGridViewTextBoxColumn.HeaderText = "Paid Amount";
+            this.paidAmountDataGridViewTextBoxColumn.Name = "paidAmountDataGridViewTextBoxColumn";
+            this.paidAmountDataGridViewTextBoxColumn.Width = 120;
+            // 
+            // balanceDataGridViewTextBoxColumn
+            // 
+            this.balanceDataGridViewTextBoxColumn.DataPropertyName = "Balance";
+            this.balanceDataGridViewTextBoxColumn.HeaderText = "Balance";
+            this.balanceDataGridViewTextBoxColumn.Name = "balanceDataGridViewTextBoxColumn";
+            this.balanceDataGridViewTextBoxColumn.Width = 96;
             // 
             // LoadReceipt
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 19F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(826, 450);
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(968, 511);
+            this.Controls.Add(this.BtnLoadReceipt);
+            this.Controls.Add(this.DGVLoadReceipt);
+            this.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
             this.Name = "LoadReceipt";
-            this.Text = "LoadReceipt";
+            this.Text = "Load Receipt";
+            this.Load += new System.EventHandler(this.LoadReceipt_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.DGVLoadReceipt)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.receiptHeaderBindingSource)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView DGVLoadReceipt;
+        private System.Windows.Forms.Button BtnLoadReceipt;
+        private System.Windows.Forms.DataGridViewTextBoxColumn receiptNoDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dateDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn timeDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn totalDiscountDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn subTotalDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn netTotalDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn paidAmountDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn balanceDataGridViewTextBoxColumn;
+        private System.Windows.Forms.BindingSource receiptHeaderBindingSource;
     }
 }
