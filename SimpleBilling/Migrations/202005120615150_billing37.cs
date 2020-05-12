b5a3c20ed7715140e20dae2a5554a7f38c7fd3f1
Mohamed Fazrin <mohamed.fazrin@staff.broadbandsolutions.com.au>
2020-05-12 11:46:45 +0530

    Applying explicit migrations: [202005120615150_billing37]. Applying explicit migration: 202005120615150_billing37. Running Seed method.

---
+++ SimpleBilling/Migrations/202005120615150_billing37.cs
@@ -0,0 +1,18 @@
+﻿namespace SimpleBilling.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class billing37 : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.GRNDetails", "IsReturned", c => c.Boolean(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.GRNDetails", "IsReturned");
+        }
+    }
+}
--- SimpleBilling/Model/GRNDetails.cs
+++ SimpleBilling/Model/GRNDetails.cs
@@ -9,19 +9,29 @@ namespace SimpleBilling.Model
         [Key]
         [Column(Order = 1)]
         public int GRN_Id { get; set; }
+
         [Key]
         [Column(Order = 2)]
         public string GRNCode { get; set; }
+
         [Key]
         [Column(Order = 3)]
         public int LineId { get; set; }
+
         public int ProductId { get; set; }
+
+        [DefaultValue(false)]
+        public bool IsReturned { get; set; }
+
         [Required]
         public float UnitCost { get; set; }
+
         [Required]
         public int Quantity { get; set; }
+
         [DefaultValue(0)]
         public float Discount { get; set; }
+
         [Required]
         public float SubTotal { get; set; }
     }
