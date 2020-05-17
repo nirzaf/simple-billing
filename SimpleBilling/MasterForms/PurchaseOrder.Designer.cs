0d0fbf7c7ea26c9232aad2f55d46f40b6e5f48e1
Mohamed Fazrin <mohamed.fazrin@staff.broadbandsolutions.com.au>
2020-05-17 07:57:50 +0530

    ReceiptNo = RcptNo,                         VehicleNumber = VehicleNo,                         Date = LblDate.Text.Trim(),                         CreatedDate = DateTime.Today

---
+++ SimpleBilling/MasterForms/PurchaseOrder.Designer.cs
@@ -0,0 +1,49 @@
+﻿namespace SimpleBilling.MasterForms
+{
+    partial class PurchaseOrder
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // PurchaseOrder
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(917, 411);
+            this.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "PurchaseOrder";
+            this.Text = "PurchaseOrder";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
