e8dc85569c44f0766490684d7622d4d6a8926fa2
Farook M Fazrin <nirzaf1@hotmail.com>
2020-06-02 23:24:01 +0530

    CAR WEST Pvt Ltd

--- SimpleBilling/SMS/Sender.cs
+++ SimpleBilling/SMS/Sender.cs
@@ -7,14 +7,14 @@ namespace SimpleBilling.SMS
 {
     public static class Sender
     {
-        private static string username = "New_city";
-        private static string password = "NCI@123";
-        private static string callerId = "CAR WEST";
+        private static string Username = "New_city";
+        private static string Password = "NCI@123";
+        private static string CallerId = "CAR WEST Pvt Ltd";
 
         public static void SendWebRequest(string DNumber, string Message)
         {
             string request = "https://bulksms2.etisalat.lk/sendsmsmultimask.php?";
-            var postData = "USER=" + HttpUtility.UrlPathEncode(username) + "&PWD=" + HttpUtility.UrlPathEncode(password) + "&MASK=" + HttpUtility.UrlPathEncode(callerId) + "&NUM=" + HttpUtility.UrlPathEncode(DNumber) + "&MSG=" + HttpUtility.UrlDecode(Message);
+            var postData = "USER=" + HttpUtility.UrlPathEncode(Username) + "&PWD=" + HttpUtility.UrlPathEncode(Password) + "&MASK=" + HttpUtility.UrlPathEncode(CallerId) + "&NUM=" + HttpUtility.UrlPathEncode(DNumber) + "&MSG=" + HttpUtility.UrlDecode(Message);
 
             using (var web = new WebClient())
             {
