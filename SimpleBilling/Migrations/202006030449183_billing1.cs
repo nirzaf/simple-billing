bc62b3fa65b2577e5b31729dd2910ae70f25fe41
Farook M Fazrin <nirzaf1@hotmail.com>
2020-06-03 10:23:38 +0530

    Carwest

---
+++ SimpleBilling/Migrations/202006030449183_billing1.cs
@@ -0,0 +1,31 @@
+﻿namespace SimpleBilling.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class billing1 : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.SMSLogs",
+                c => new
+                    {
+                        LogKey = c.String(nullable: false, maxLength: 128),
+                        LogMessage = c.String(),
+                        Number = c.String(),
+                        Log = c.String(),
+                        CreatedDate = c.DateTime(),
+                        UpdatedDate = c.DateTime(),
+                        IsDeleted = c.Boolean(nullable: false),
+                    })
+                .PrimaryKey(t => t.LogKey);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.SMSLogs");
+        }
+    }
+}
