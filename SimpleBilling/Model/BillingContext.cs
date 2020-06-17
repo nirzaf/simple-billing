d908f7fc60356701bd98d44cafa50c17de04e1d0
Farook M Fazrin <nirzaf1@hotmail.com>
2020-06-17 20:34:07 +0530

    Customer Deleted Successfully.....

--- SimpleBilling/Model/BillingContext.cs
+++ SimpleBilling/Model/BillingContext.cs
@@ -5,9 +5,9 @@ namespace SimpleBilling.Model
     public class BillingContext : DbContext
     {
         /// <summary>
-        /// Version 1.0.1.0
+        /// Version 1.0.1.1
         /// </summary>
-        public BillingContext() : base("name=con1")
+        public BillingContext() : base("name=con")
         {
             Database.SetInitializer(new BillingDBInitializer());
         }
